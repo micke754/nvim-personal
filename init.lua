74fea77ed13f8ab51820796ccb904321704d55ff
kmichaels <kmichaels@kmichaelss-MacBook-Air.local>
2024-07-20 17:39:10 +1200

    Have restructured everything to work such that cor-plugins get loaded automatically from the lazy-plugins file

--- init.lua
+++ init.lua
@@ -1,20 +1,31 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 require("options")
-
-require("lazy").setup("plugins")
-
+require("lazy-plugins")
 require("keymaps")
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.background = "light"
+-- vim.g.mapleader = " "
+-- vim.g.background = "light"
 -- vim.g.colorscheme = "retrobox"
 
 vim.opt.swapfile = false
